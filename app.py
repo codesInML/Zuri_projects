1fda4ffe1b4f0597ca27d2b7822b648f970d7fc1
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2021-04-15 11:25:58 +0100

    made a few changes to the app file

--- app.py
+++ app.py
@@ -23,9 +23,11 @@ def login():
 
         else:
             print('Password is incorrect, please try again')
+            login()
 
     else:
         print('Name not found, please try again')
+        login()
 
 
 def register():
@@ -44,35 +46,47 @@ def register():
         register()
 
 
-def options(name, id):
-    print(f'Date: {date}')
-    print('\n')
-    print(f'Welcome {name}, your account number is {accout_number[id]}')
+def bankOperations():
     print('These are the available options:')
     print('1. Withdrawal')
     print('2. Cash Deposit')
     print('3. Complaint')
-
+    print('4. Logout')
     selected_option = int(input('Please select an option: '))
 
     if selected_option == 1:
         withdraw_amount = input(
             f"How much would you like to withdraw? #")
         print(
-            f"Take your cash, you withdrew {withdraw_amount}. \nThank you for banking with us.")
+            f"Take your cash, you withdrew {withdraw_amount}. \nThank you for banking with us. \n\n")
+        bankOperations()
 
     elif selected_option == 2:
         deposit_amount = input(
             f"How much would you like to deposit? #")
         print(
-            f"Current Balance {deposit_amount}. \nThank you for banking with us.")
+            f"Current Balance {deposit_amount}. \nThank you for banking with us. \n\n")
+        bankOperations()
 
     elif selected_option == 3:
         complaint = input('What issue would you like to report: \n')
-        print("Thank you for contacting us.")
+        print("Thank you for contacting us. \n\n")
+        bankOperations()
+
+    elif selected_option == 4:
+        exit()
 
     else:
-        print('Invalid Option selected, please try again.')
+        print('Invalid Option selected, please try again. \n\n')
+        bankOperations()
+
+
+def options(name, id):
+    print(f'Date: {date}')
+    print('\n')
+    print(f'Welcome {name}, your account number is {accout_number[id]}')
+
+    bankOperations()
 
 
 def atm():
